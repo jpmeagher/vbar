c35af932b49a3c642a3f0bcc5d6304c72618e72b
jpmeagher <J.Meagher@warwick.ac.uk>
2021-07-14 16:31:42 +0100

    pass R cmd check after writing ou_kernel

--- R/kernels.R
+++ R/kernels.R
@@ -1,7 +1,7 @@
 #' The Ornstein-Uhlenbeck kernel
 #'
-#' Computes \deqn{amplitude * \exp \left( - \frac{d}{length_scale} \right)} where
-#' \eqn{d \geq 0} is the distance between any two points in space for \eqn{amplitude > 0} and
+#' Computes \deqn{amplitude * \exp ( - d / length_scale )} where
+#' \eqn{d \ge 0} is the distance between any two points in space for \eqn{amplitude > 0} and
 #' \eqn{length_scale > 0}.
 #'
 #' @param d A set of non-negative real numbers. Pairwise distances between points.
@@ -9,7 +9,7 @@
 #' @param length_scale A positive real number. The length-scale of the Ornstein-Uhlenbeck process.
 #' @param perform_checks Logical. Check if function inputs are specified correctly.
 #'
-#' @return An array of positive values between 0 and \texttt{amplitude} with the same dimension as d.
+#' @return An array of positive values between 0 and \eqn{amplitude} with the same dimension as d.
 #' @export
 #'
 #' @examples
