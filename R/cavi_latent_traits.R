1e3ebac0d4b9e56f3616d271faff1315e1314def
jpmeagher <J.Meagher@warwick.ac.uk>
2021-07-16 12:50:47 +0100

    add latent trait outer product function

--- R/cavi_latent_traits.R
+++ R/cavi_latent_traits.R
@@ -1,11 +1,29 @@
-update_latent_traits <- function(
-  auxiliary_trait_expectation,
-  loading_expectation, loading_variance,
-  factor_expectation, factor_variance,
-  phy,
-  precision_expectation,
+#' Compute latent trait outer product expectations
+#'
+#' Computes the expected value of the outer product of the \eqn{L} latent traits for the \eqn{n^{th}} individual under the approximate posterior distribution.
+#'
+#' @param z_n_tilde A real-valued vector length \eqn{L}. The expected latent trait under the approximate posterior distribution.
+#' @param S_n_z A \eqn{L \times L} covariance matrix. The variance of the latent trait under the approximate posterior distribution.
+#' @inheritParams ou_kernel
+#'
+#' @return A symmetric \eqn{L \times L} matrix.
+#' @export
+#'
+#' @examples
+#' L <- 5
+#' z <- rnorm(L)
+#' S <- stats::rWishart(1, df = L, Sigma = diag(L)/ L^2)[, , 1]
+#' latent_trait_outer_product_expectation(z_n_tilde = z, S_n_z = S)
+latent_trait_outer_product_expectation <- function(
+  z_n_tilde, S_n_z,
   perform_checks = TRUE
 ){
-
+  if (perform_checks) {
+    L <- length(z_n_tilde)
+    checkmate::assert_numeric(z_n_tilde, any.missing = FALSE, finite = TRUE)
+    checkmate::assert_matrix(S_n_z, mode = "numeric", any.missing = FALSE, nrows = L, ncols = L)
+    checkmate::assert_true(isSymmetric(S_n_z))
+  }
+  S_n_z + z_n_tilde %*% t(z_n_tilde)
 }
 
